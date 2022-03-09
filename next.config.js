f84b6fdb0d3db8404eba4ba402c470dfc8eca1d4
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-03-09 19:32:36 +0100

    :hammer: chore: bump eslint packages

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ module.exports = {
     if (!dev && !isServer) {
       config.resolve.alias = {
         ...config.resolve.alias,
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
         react: "preact/compat",
         "react-dom/test-utils": "preact/test-utils",
         "react-dom": "preact/compat",
