25f423ae489f1a8c2f2c3940da4c624e384cab44
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-08-02 08:52:29 +0200

    :hammer: chore: bump deps

--- next.config.js
+++ next.config.js
@@ -6,17 +6,4 @@
 module.exports = {
   cleanDistDir: true,
   reactStrictMode: true,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      };
-    }
-
-    return config;
-  },
 };
